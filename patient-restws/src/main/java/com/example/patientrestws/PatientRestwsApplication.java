e71ea244d40e7f4d25908d36b111a3f4b35ba1bf
Karim Abo Agiza <kemoaboagiza26112000@gmail.com>
2023-10-14 15:32:00 +0300

    adding 2 tasks

---
+++ patient-restws/src/main/java/com/example/patientrestws/PatientRestwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.patientrestws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRestwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientRestwsApplication.class, args);
+    }
+
+}
---
+++ patient-restws/src/test/java/com/example/patientrestws/PatientRestwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patientrestws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientRestwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
